5201999c24f1907efdb4960083b022048dc46074
Chris Vasselli <clindsay@gmail.com>
2020-05-02 17:59:41 -0400

    Add test code and images.

--- TestCoreImageOutput/ViewController.swift
+++ TestCoreImageOutput/ViewController.swift
@@ -10,9 +10,31 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var imageView: UIImageView!
+    
+    // MarioSRGB.png -              Works
+    // MarioDisplayP3.png -         Does Not Work   -- works if I change output color space to displayP3
+    // door-warningSRGB.jpg -       Does Not Work
+    // door-warningSRGB.png -       Works
+    // door-warningDisplayP3.jpg -  Does Not Work   -- does not work even with output color space set to displayP3
+    // stardew-valleySRGB.jpg -     Does Not Work
+    // stardew-vallySRGB.png -      Works
+    // stardew-vallyDisplayP3.png - Does Not Work
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        let photoURL = Bundle.main.url(forResource: "MarioSRGB", withExtension: "png")!
+        
+        let ciImage = CIImage(contentsOf: photoURL, options: [.applyOrientationProperty: true])!
+
+        let outputURL = FileManager.default.temporaryDirectory.appendingPathComponent("test.jpeg")
+        
+        let context = CIContext()
+        try! context.writeJPEGRepresentation(of: ciImage, to: outputURL, colorSpace: CGColorSpace(name: CGColorSpace.sRGB)!, options: [:])
+
+        let loadedImage = UIImage(contentsOfFile: outputURL.path)
+        imageView.image = loadedImage
     }
 
 
