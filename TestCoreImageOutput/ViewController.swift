2a3d68d5391e4616437d882cfc24cad4ae55fb26
Chris Vasselli <clindsay@gmail.com>
2020-05-02 17:20:53 -0400

    Initial Commit

---
+++ TestCoreImageOutput/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TestCoreImageOutput
+//
+//  Created by Chris Vasselli on 5/2/20.
+//  Copyright © 2020 Serpenti Sei. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
